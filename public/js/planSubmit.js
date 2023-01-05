44c0c4c0d56ccfd2b2614005f6559b46a6170b0f
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2023-01-04 21:07:48 -0600

    collects the end date from user input

--- public/js/planSubmit.js
+++ public/js/planSubmit.js
@@ -4,8 +4,7 @@ const planSubmit = async (event) => {
     const plan_name = document.querySelector('#planName').value.trim();
     const description = document.querySelector('#description').value.trim();
     const startDate = new Date(document.querySelector('#startDate').value);
-
-    console.log(plan_name, description, startDate.toISOString());
+    const endDate = new Date(document.querySelector('#endDate').value);
 
     await fetch('/api/planner', {
         method: 'POST',
@@ -17,7 +16,7 @@ const planSubmit = async (event) => {
             plan_name,
             description,
             start_date: startDate.toISOString(),
-            end_date: 2024
+            end_date: endDate.toISOString()
         })
     })
     .catch(err => console.error('Error:', err));
