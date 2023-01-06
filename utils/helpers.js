a7f24b544db19d220d27a8b1e91b02ff147f001e
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2023-01-06 16:49:40 -0600

    helper method for route

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,8 @@
 module.exports = {
     format_date: (date) => {
         return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    },
+    view_plan_link: (id) => {
+        return `plan/${id}/comments`
     }
 };
